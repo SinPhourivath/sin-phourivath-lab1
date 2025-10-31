0e0a3c128d60624ccf2c159be0c2557cdc7620d9
Rivath <163083945+SinPhourivath@users.noreply.github.com>
2025-10-31 15:54:27 +0700

    Add code coverage test and archieve

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", (c) => {
-	return c.text("Hello, CI/CD!");
+	return c.text("Hello, GitHub Actions!");
 });
 
 export default app;
