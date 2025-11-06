cbbe874870ea19e8fb0301b79d35d87626c66dfb
Rivath <163083945+SinPhourivath@users.noreply.github.com>
2025-11-06 20:16:10 +0700

    Add another route

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,9 @@ app.get("/", (c) => {
 	return c.text("Hello, GitHub Actions!");
 });
 
+app.get("/:name", (c) => {
+	const name = c.req.param("name");
+	return c.text(`Hello, ${name}!`);
+});
+
 export default app;
